c45e13d01f8c064fd34124610e736e5df37f702e
Rutik1701 <parmarrutik882@gmail.com>
2026-01-22 16:01:05 +0530

    library

---
+++ font_manager/lib/font_manager.dart
@@ -0,0 +1,7 @@
+library font_manager;
+
+export 'src/font_button.dart';
+export 'src/font_input.dart';
+export 'src/font_manager.dart';
+export 'src/font_provider.dart';
+export 'src/font_text.dart';
